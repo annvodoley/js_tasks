17250a6a18455147f095b1a6579f74a2555bbb81
annvodoley <roxy-222@mail.ru>
2022-04-20 22:21:13 +0300

    add task 8

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 //методы массивов
 
-//task 7
+//task 8
 
 const arr = [1, 2, 3, 4, 5]
-console.log(arr.slice(0,3));
-console.log(arr.slice(3,6));
+arr.splice(1, 2)
+console.log(arr);
+
+const arr1 = [1, 2, 3, 4, 5]
+let arr2 = arr1.splice(1, 3)
+console.log(arr2)
 
