91f0663c841362f7779792f13c602fde9529bdc7
annvodoley <roxy-222@mail.ru>
2022-04-20 22:19:47 +0300

    add task 7

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 //методы массивов
 
-//task 6
+//task 7
 
-const arr = ['js', 'css', 'jq']
-console.log(arr[2]);
-console.log(arr[0]);
+const arr = [1, 2, 3, 4, 5]
+console.log(arr.slice(0,3));
+console.log(arr.slice(3,6));
 
